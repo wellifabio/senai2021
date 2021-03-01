8a31062c85f9604b9a4c350ed68459fa38ca42a2
rafaelalvesmartins <rafaelmartinsalves@gmail.com>
2021-03-01 08:56:43 -0300

    exemplo fpoo

---
+++ 1des/fpoo/Aula/02/Exemplos/saida.py
@@ -0,0 +1,8 @@
+var1 = 5
+var2 = 10
+
+media = (var1+var2)/2
+
+print("Media entre variavel 1:",var1," e variavel 2:",var2," eh: ", media)
+print("Media entre variavel 1: %d e variavel 2: %d  eh: %f" %(var1,var2,media))
+print(f"Media entre variavel 1: {var1} e variavel 2: {var2}  eh: {media}")
