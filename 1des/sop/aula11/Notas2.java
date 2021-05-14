4de0e6ccdd15bf02d94b38d812f50246e93791c3
wellifabio <wellifabio@hotmail.com>
2021-05-14 09:45:46 -0300

    3DES add aula21 e 22

--- 1des/sop/aula11/Notas2.java
+++ 1des/sop/aula11/Notas2.java
@@ -1,105 +1,83 @@
-/*
-Exercício de Lógica
-Programa que lê a partir do teclado um valor de ponto
-flutuante com duas casas decimais (monetário) e calcula
-o menor número de notas (cédulas) e ou moedas no qual o
-valor pode ser decomposto. As notas são 100, 50, 20, 10, 5, 2
-e as moedas são 1, 0.5, 0.25, 0.1, 0.05. A seguir
-mostre o valor lido e a relação de notas necessárias.
-*/
-
+import java.io.IOException;
 import java.util.Scanner;
-
-class Notas2 {  
+import java.util.Locale;
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Notas2 {
+ 
   public static void main(String args[]) {
+
     //Declaração de variáveis
+    Locale.setDefault(Locale.US);
     Scanner entrada = new Scanner(System.in);
     float valor = 0;
     int cem = 0, cinquenta = 0, vinte = 0, dez = 0, cinco = 0, dois = 0;
     int um = 0, mCinquenta = 0, mVinteECinco = 0, mDez = 0, mCinco = 0;
     int mUm = 0;
+    
     //Entrada
-    System.out.println("Digite um valor em dinheiro, maior do que zero:");
     valor = entrada.nextFloat();
-
+	
     //Processamento
     do{
       if(valor >= (float)100){
         cem++;
-        valor -= (float)100;
+        valor = valor - (float)100;
       } else if(valor >= (float)50){
         cinquenta++;
-        valor -= (float)50;
+        valor = valor - (float)50;
       } else if(valor >= (float)20){
         vinte++;
-        valor -= (float)20;
+        valor = valor - (float)20;
       } else if(valor >= (float)10){
         dez++;
-        valor -= (float)10;
+        valor = valor - (float)10;
       } else if(valor >= (float)5){
         cinco++;
-        valor -= (float)5;
+        valor = valor - (float)5;
       } else if(valor >= (float)2){
         dois++;
-        valor -= (float)2;
+        valor = valor - (float)2;
       } else if(valor >= (float)1){
         um++;
-        valor -= (float)1;
+        valor = valor - (float)1;
       } else if(valor >= (float)0.5){
         mCinquenta++;
-        valor -= (float)0.5;
+        valor = valor - (float)0.5;
       } else if(valor >= (float)0.25){
         mVinteECinco++;
-        valor -= (float)0.25;
+        valor = valor - (float)0.25;
       } else if(valor >= (float)0.1){
         mDez++;
-        valor -= (float)0.1;
+        valor = valor - (float)0.1;
       } else if(valor >= (float)0.05){
         mCinco++;
-        valor -= (float)0.05;
+        valor = valor - (float)0.05;
       }else{
         mUm++;
-        valor -= (float)0.01;
+        valor = valor - (float)0.01;
       }
     }while(valor > (float)0.00);
 
     //Saídas
-    System.out.println("Será necessário:");
-    if(cem > 0){
-      System.out.println(cem+" notas de Cem;");
-    }
-    if(cinquenta > 0){
-      System.out.println(cinquenta+" notas de Cinquenta;");
-    }
-    if(vinte > 0){
-      System.out.println(vinte+" notas de Vinte;");
-    }
-    if(dez > 0){
-      System.out.println(dez+" notas de Dez;");
-    }
-    if(cinco > 0){
-      System.out.println(cinco+" notas de Cinco;");
-    }
-    if(dois > 0){
-      System.out.println(dois+" notas de Dois;");
-    }
-    if(um > 0){
-      System.out.println(um+" moedas de Um;");
-    }
-    if(mCinquenta > 0){
-      System.out.println(mCinquenta+" moedas de Cinquenta centavos;");
-    }
-    if(mVinteECinco > 0){
-      System.out.println(mVinteECinco+" moedas de Vinte e cinco centavos;");
-    }
-    if(mDez > 0){
-      System.out.println(mDez+" moedas de Dez centavos;");
-    }
-    if(mCinco > 0){
-      System.out.println(mCinco+" moedas de Cinco centavos;");
-    }
-    if(mUm > 0){
-      System.out.println(mUm+" moedas de Um centavo;");
-    }
-  } 
+    System.out.println("NOTAS:");
+    System.out.println(cem+" nota(s) de R$ 100.00");
+    System.out.println(cinquenta+" nota(s) de R$ 50.00");
+    System.out.println(vinte+" nota(s) de R$ 20.00");
+    System.out.println(dez+" nota(s) de R$ 10.00");
+    System.out.println(cinco+" nota(s) de R$ 5.00");
+    System.out.println(dois+" nota(s) de R$ 2.00");
+    System.out.println("MOEDAS:");
+    System.out.println(um+" moeda(s) de R$ 1.00");
+    System.out.println(mCinquenta+" moeda(s) de R$ 0.50");
+    System.out.println(mVinteECinco+" moeda(s) de R$ 0.25");
+    System.out.println(mDez+" moeda(s) de R$ 0.10");
+    System.out.println(mCinco+" moeda(s) de R$ 0.05");
+    System.out.println(mUm+" moeda(s) de R$ 0.01");
+  }
+ 
 }
