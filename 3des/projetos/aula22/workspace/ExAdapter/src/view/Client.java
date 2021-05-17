f191b257546a638b1be924d4e9eaa58c53813b67
wellifabio <wellifabio@hotmail.com>
2021-05-17 11:28:05 -0300

    3des projetos aula22 workspace adapter

---
+++ 3des/projetos/aula22/workspace/ExAdapter/src/view/Client.java
@@ -0,0 +1,31 @@
+package view;
+
+///* 
+//* Exemplo de uma Cliente que implementa os
+//* Objetos que vão efetivamente utilizar os
+//* recursos de CRUD nos dois SGBDs
+//* */
+
+import javax.swing.JOptionPane;
+
+import model.MySQLAdapter;
+import model.MySQLCommands;
+import model.OracleCommands;
+
+public class Client {
+
+	static OracleCommands oracle;
+	static MySQLAdapter mysql;
+
+	public static void main(String[] args) {
+		
+		//Utilização direta, sem o Patter Adapter
+		oracle = new OracleCommands();
+		JOptionPane.showMessageDialog(null, oracle.insert());
+		
+		//Utilização com o Patter Adapter, MySQLAdapter
+		mysql = new MySQLAdapter(new MySQLCommands());
+		JOptionPane.showMessageDialog(null, mysql.insert());
+	}
+
+}
