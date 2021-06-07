9bcadc939126d9a2066741cbac72e284891fc0d0
Wellington Martins <wellington.martins@bentoquirino.edu>
2021-06-07 12:34:28 -0300

    A

--- 3des/projetos/aula22/workspace/EstruturaFacade/src/client/CaixaEletronico.java
+++ 3des/projetos/aula22/workspace/EstruturaFacade/src/client/CaixaEletronico.java
@@ -6,13 +6,13 @@ import process.ContaCorrenteFacade;
 
 /*
  * Facade ou Fachada
- * Serve para criar uma interface simplificada que roda outras ações por trás.
+ * Serve para criar uma interface simplificada que roda outras aï¿½ï¿½es por trï¿½s.
  * Exemplo:
- * Para sacar R$ 50,00 do Banco, seria necessário:
- * Validar o número da conta
+ * Para sacar R$ 50,00 do Banco, seria necessï¿½rio:
+ * Validar o nï¿½mero da conta
  * Validar a senha
  * Validar se tem saldo na conta
- * Realizar a alteração do saldo da conta
+ * Realizar a alteraï¿½ï¿½o do saldo da conta
  * https://www.youtube.com/watch?v=B1Y8fcYrz5o
  * */
 
@@ -20,20 +20,19 @@ public class CaixaEletronico {
 
 	public static ContaCorrenteFacade cc;
 	public static int menu;
-
 	public static Scanner leia = new Scanner(System.in);
 
 	public static void main(String[] args) {
 
 		menu = 0;
-		System.out.print("Digite o número da conta e a senha:");
+		System.out.print("Digite o nï¿½mero da conta e a senha:");
 		cc = new ContaCorrenteFacade(leia.nextInt(),leia.nextInt());
 				
 		while (menu != 3) {
-			System.out.print("\n\n1.Depositar\n2.Sacar\n3.sair\nDigite uma opção:");
+			System.out.print("\n\n1.Depositar\n2.Sacar\n3.sair\nDigite uma opï¿½ï¿½o:");
 			switch (menu = leia.nextInt()) {
 			case 1:
-				System.out.print("Digite o valor do depósito:");
+				System.out.print("Digite o valor do depï¿½sito:");
 				cc.depositar(leia.nextDouble());
 				break;
 			case 2:
@@ -44,7 +43,7 @@ public class CaixaEletronico {
 				System.out.println("Valews, Falows.");
 				break;
 			default:
-				System.out.println("Opção inválida.");
+				System.out.println("Opï¿½ï¿½o invï¿½lida.");
 				break;
 			}
 		}
