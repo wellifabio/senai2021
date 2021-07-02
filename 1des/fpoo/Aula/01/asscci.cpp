4f6c7f82a078c5a36f5a14beed8d1a0670f17d5c
wellifabio <wellifabio@hotmail.com>
2021-07-02 11:46:43 -0300

    1des fpoo questionario final

---
+++ 1des/fpoo/Aula/01/asscci.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int x = 10;
+	int y = 20;
+	int z = x + y;
+	char k = 'a';
+	printf("O caracter k = %c \n",k);
+	printf("O ASSCII k = %i \n",k);
+	printf("O endereço hexadecimal de k = %x \n",&k);
+	printf("A soma de x + y = %i",z);
+}
