353730ca3cd5506f5fc500474c22e5c400832a82
Aluno <reenyelima@hotmail.com>
2021-07-29 11:30:33 -0300

    2des bd aula1

---
+++ 2des/bcd/aula01/script.sql
@@ -0,0 +1,14 @@
+-- Comando que mostra todos os bancos de dados do SGBD
+show databases;
+-- Comando para entrar em um Banco de dados
+use nomebd;
+-- Comando para ver o que tem dentro do banco de dados (Tabelas)
+show tables;
+-- Comando para ver o que tem em uma tabela (Listar a tabela inteira)
+select * from db;
+-- Comando para sair
+exit;
+
+MySQL é um SGBD (Sistema Gerenciador de Banco de Dados)
+Banco de Dados é um arquivo com dados ou informções (Dado é bruto e informação é refinada)
+Normalmente Banco de dados é estruturado em tabelas
