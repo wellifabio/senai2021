d598700fba196cab776c0ef6c045ed7a07df01d7
Reenye Lima <52282158+ReenyeLima@users.noreply.github.com>
2021-08-02 09:10:36 -0300

    aula01

---
+++ 2des/pwfe/Aula 01/aula01/index.js
@@ -0,0 +1,126 @@
+/*
+    var - Variavel Global
+    let - Variavel Local
+    const - Constante
+*/
+
+/*
+    console.log(); - Imprime um dado no console do navegador;    
+*/
+
+/*
+    Soma            +
+    Subtracao       -
+    Divisão         /
+    Multiplicação   *
+    Potencia        **
+    Resto           %     
+*/
+
+/*
+    Maior               >
+    Menor               <
+    Maior Igual         >=
+    Menor Igual         <=
+    Igual               ==
+    Diferente           !=
+    Igualdade Restrita  ===
+    Diferença Restrita  !==
+*/
+
+/*
+    AND && 
+    OR  ||
+    NOT !
+    
+    TRUE && TRUE    => TRUE
+    TRUE && FALSE   => FALSE
+    FALSE && TRUE   => FALSE
+    FALSE && FALSE  => FALSE
+
+    TRUE || TRUE    => TRUE
+    TRUE || FALSE   => TRUE
+    FALSE || TRUE   => TRUE
+    FALSE || FALSE  => FALSE
+
+    !FALSE  => TRUE
+    !TRUE   => FALSE
+*/
+
+/*var data = new Date();
+
+var dataatual = data.getDate() + "/" + (data.getMonth() + 1) + "/" + data.getFullYear();
+
+//Template String
+// `Texto normal até a variavel ${sua_variavel}`
+dataatual = `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
+
+var horaatual = `${data.getHours()}:${data.getMinutes()}`;
+
+console.log(`${dataatual} ${horaatual}`);*/
+
+//Math.ceil() arredonda pra cima
+//Math.floor() arredonda pra baixo
+//Math.round() arredonda de acordo com a casa decimal
+//Math.max() retorna o maior valor de uma sequencia
+//Math.min() retorna o menor valor de uma sequencia
+
+//Math.abs() retorna valor absoluto
+
+//Number.parseInt() Converte uma string para inteiro
+//Number.parseFloat() converte uma string para float
+
+//replace() Substitui a primeira ocorrencia
+//replaceAll() Substitui todos caracteres
+
+/*if() {
+
+}else if() {
+
+}else {
+
+}
+
+switch() {
+    case :
+        break;
+    default :
+        break;
+}
+
+for(let i = 0; i < 10; i++) {
+
+}
+
+while() {
+
+}
+
+do {
+
+}while();*/
+
+//for in -> retorna o indice
+//for of -> retorna o valor == foreach
+
+//let arr = "a b c d e".split(" ");
+
+/*for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}*/
+
+/*arr.forEach(item => {
+    console.log(item);
+});
+
+for (let item of arr) {
+    console.log(item);
+}
+
+for (let item if arr) {
+    console.log(item);
+}*/
+
+function ex1() {
+    console.log("EXECUTANDO A FUNCAO EX1");
+}
