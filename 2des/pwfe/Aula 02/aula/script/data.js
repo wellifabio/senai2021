d234fabd72fd949537e85441b00e1dd65ba1795b
Reenye Lima <52282158+ReenyeLima@users.noreply.github.com>
2021-08-03 09:59:22 -0300

    Aula 02

---
+++ 2des/pwfe/Aula 02/aula/script/data.js
@@ -0,0 +1,26 @@
+// var n1 = document.querySelector("#nota1");
+// var n2 = document.querySelector("#nota2");
+// var media = document.querySelector("#media");
+
+// function calcularMedia() {
+//     let nota1 = Number(n1.value);
+//     let nota2 = Number(n2.value);
+//     let calc = (nota1 + nota2) / 2;
+//     media.innerHTML = "A media e : " + calc;
+// }
+
+// var notas = document.querySelectorAll("input");
+
+// function calcularMedia() {
+//     let nota = 0;
+//     let div = notas.length;
+
+//     notas.forEach(item => {
+//         if (item.value == "") div--;
+//         nota = nota + Number(item.value);
+//     });
+
+//     let media = nota / div;
+
+//     document.querySelector("#media").innerHTML = "Resultado : " + media;
+// }
---
+++ 2des/pwfe/Aula 02/aula/script/index.js
@@ -0,0 +1,26 @@
+/*var body = document.querySelector("body");
+var title = document.querySelector("h1");
+var box = document.querySelector(".box");
+var caixa = document.querySelector("#caixa");
+var div = document.querySelectorAll("div");
+
+body.style.backgroundColor = "red";
+
+title.innerHTML = "Titulo JS";
+
+box.innerHTML = "Minha div";
+
+caixa.innerHTML = "TESTE CAIXA";
+
+console.log(div);
+
+var title = document.getElementById("title");
+title.innerHTML = "TEXTO TESTE";
+console.log(title);
+
+var box = document.getElementsByClassName("box");
+box[0].innerHTML = "TESTE";
+
+var body = document.getElementsByTagName("div");
+
+console.log(body);*/
