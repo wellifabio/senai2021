9f27ec24d8b0afadd531e440013f795cc85b7b38
Reenye Lima <reenyelima@hotmail.com>
2021-08-17 10:34:50 -0300

    atualizacao

--- 2des/pwfe/Aula 06/correção/index.js
+++ 2des/pwfe/Aula 06/correção/index.js
@@ -3,6 +3,7 @@ var endereco = document.querySelector("#endereco");
 var lanche = document.querySelector("#lanche");
 
 var prod = document.querySelector(".producao");
+var entrega = document.querySelector(".entrega");
 
 function lancarpedido() {
     let card = document.createElement("div");
@@ -28,6 +29,13 @@ function lancarpedido() {
 
     let button = document.createElement("button");
     button.innerHTML = "Enviar para entrega";
+    button.addEventListener("click", () => {
+        entrega.appendChild(card);
+        button.innerHTML = "Pedido entregue";
+        button.addEventListener("click", () => {
+            card.remove();
+        });
+    });
 
     card.appendChild(data);
     card.appendChild(planche);
