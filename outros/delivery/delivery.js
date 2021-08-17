c112c9976f01a912ef5b8bb953120ac0a582ffea
Aluno <reenyelima@hotmail.com>
2021-08-17 11:29:23 -0300

    correcoes

---
+++ outros/delivery/delivery.js
@@ -0,0 +1,7 @@
+const cliente = document.querySelector("#cliente")
+const endereco = document.querySelector("#endereco")
+const produto = document.querySelector("#produto")
+
+function addProducao(){
+    alert(cliente.value + endereco.value + produto.value)
+}
