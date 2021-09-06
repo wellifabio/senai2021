cdea5deb785f3a5555e1c6841a54c169b3c074c2
Aluno <reenyelima@hotmail.com>
2021-09-06 09:35:18 -0300

    2des bcd aula04 society

--- 2des/bcd/aula05/agencias.sql
+++ 2des/bcd/aula05/agencias.sql
@@ -2,6 +2,7 @@ drop database if exists agencia;
 create database agencia;
 use agencia;
 
+-- DDL (Data Develpoment Language)
 create table vendedores(
     id_vendedor integer not null primary key auto_increment,
     nome VARCHAR(40) not null
@@ -30,3 +31,42 @@ create table vendas(
 );
 
 show tables;
+
+-- DML (Data Manipulation Language)
+-- CRUD em MYSQL (INSERT INTO, SELECT, UPDATE e DELETE)
+
+describe agencias;
+insert into agencias(cidade,uf) values
+("Jaguariúna","SP"),
+("Pedreira","SP"),
+("Holambra","SP"),
+("Amparo","SP"),
+("Serra Negra","SP");
+select * from agencias;
+
+describe vendedores;
+insert into vendedores(nome) values ("Jair"),("Jafoi"),("Jaera"),("Javai");
+select * from vendedores;
+describe veiculos;
+insert into veiculos(modelo,preco) values
+("Palio",9000.00),
+("Gol",17000.00),
+("Fiesta",19000.00),
+("Celta",29000.00);
+select * from veiculos;
+insert into veiculos(modelo,preco) values
+("Palio",9000.00),
+("Gol",17000.00),
+("Fiesta",19000.00),
+("Celta",29000.00);
+select * from veiculos;
+select modelo from veiculos;
+select preco from veiculos;
+select modelo, preco from veiculos;
+
+-- Apaga o conteúdo da tabela inteira
+delete from veiculos;
+-- Aaltera o conteúdo da tabela inteira
+update veiculos set modelo = "Civic";
+
+-- WHERE (Onde) Cláusula mais importante no "CRUD"
