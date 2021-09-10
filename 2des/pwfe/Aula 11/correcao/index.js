dedc1342eb124520519abb8e8a53a6bdcc4ced1b
wellifabio <wellifabio@hotmail.com>
2021-09-10 08:06:29 -0300

    pwfe corrigido

--- 2des/pwfe/Aula 11/correcao/index.js
+++ 2des/pwfe/Aula 11/correcao/index.js
@@ -1,8 +1,9 @@
 function add() {
     let corpo = document.querySelector(".corpo")
     let card = document.createElement("div")
+    let comp = document.getElementById("compromisso")
     card.className = "card"
-    card.innerHTML = "<label>"+document.getElementById("compromisso").value+"</label>"
+    card.innerHTML = "<label>"+comp.value+"</label>"
     let importante = document.createElement("button")
     importante.className = "importante"
     importante.addEventListener("click", () => {
@@ -16,4 +17,5 @@ function add() {
     card.appendChild(importante)
     card.appendChild(excluir)
     corpo.appendChild(card)
+    comp.value = ""
 }
