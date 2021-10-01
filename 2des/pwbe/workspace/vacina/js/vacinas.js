0a5c20759826c53daaeca6ca8ccd4815be73a743
Aluno <reenyelima@hotmail.com>
2021-10-01 10:34:56 -0300

    2des pwbq delivery org

--- 2des/pwbe/workspace/vacina/js/vacinas.js
+++ 2des/pwbe/workspace/vacina/js/vacinas.js
@@ -19,49 +19,62 @@ function vacinacao(dados) {
         aplica(vetor[i + 1]);
     }
 
-    resultado += "\nPessoas completamente imunizadas: " + d2
-    resultado += "\nPessoas imunizadas apenas com uma dose: " + d1
-    resultado += "\nPessoas que tomaram a segunda dose com atraso: " + d2a
-    resultado += "\nPessoas esperando a segunda dose com atraso: " + d1a
+    resultado += "Pessoas completamente imunizadas: " + d2
+    resultado += "\r\nPessoas imunizadas apenas com uma dose: " + d1
+    resultado += "\r\nPessoas que tomaram a segunda dose com atraso: " + d2a
+    resultado += "\r\nPessoas esperando a segunda dose com atraso: " + d1a
 
-    console.log(resultado)
+    download(resultado)
+
 }
 
-function aplica(lote) {
-    console.log("Lote = "+lote);
+function download(dados){
+    let a = document.createElement("a");
+    a.href = "data:," + dados;
+    a.download = "saida.txt";
+    a.click();
+}
+
+function aplica(vacinas) {
+    let lote = parseInt(vacinas)
     //Primeiro, se existem pessoas com a segunda dose em atraso
-    if(d1a > 0){
+    if (d1a > 0) {
         //verifico se as doses são suficientes
-        if(d1a <= lote){
-            lote -= d1a //Retiro do lote as doses aplicadas
-            d2a =+ d1a; //Acrecenta em quem tomou atrazado
+        if (d1a <= lote) {
             d2 += d1a; //Acrecenta em imunizados
-            d1a = 0; //Todos os atrazados vacinados
-        }else{
+            lote -= d1a //Retiro do lote as doses aplicadas
+            d2a += d1a; //Acrecenta em quem tomou atrazado
+            d1 -= d1a; //Retira também de quem tomou a primeira dose
+            d1a = 0; //Todos os atrazados vacinados            
+        } else {
             d1a -= lote; //Aplico todas as doses
-            d2a =+ lote; //Acrecenta em quem tomou atrazado
+            d2a += lote; //Acrecenta em quem tomou atrazado
             d2 += lote; //Acrecenta em imunizados
+            d1 -= lote //Remover de aguardando segunda dose
             lote = 0; //Esvazio o lote
         }
     }
 
-    //Em seguida, são vacinadas as pessoas com a segunda dose em dia
-    if(d1 > 0){
-        //verifico se as doses são suficientes
-        if(d1 <= lote){
-            lote -= d1 //Retiro do lote as doses aplicadas
-            d2 =+ d1; //Acrecenta em quem tomou atrazado
-            d1 = 0; //Todos os atrazados vacinados
-        } else {
-            d1 -= lote; //Aplico todas as doses
-            d2 += lote; //Acrecenta em imunizados
-            lote = 0; //Esvazio o lote
+    //Verifica se ainda há vacinas
+    if (lote > 0) {
+        //Em seguida, são vacinadas as pessoas com a segunda dose em dia
+        if (d1 > 0) {
+            //verifico se as doses são suficientes
+            if (d1 <= lote) {
+                d2 += d1; //Acrecenta em quem tomou atrazado
+                lote -= d1; //Retiro do lote as doses aplicadas
+                d1 = 0; //Todos os atrazados vacinados
+            } else {
+                d1 -= lote; //Aplico todas as doses
+                d2 += lote; //Acrecenta em imunizados
+                d1a += d1; //Acrescenta aos atrasados
+                lote = 0; //Esvazio o lote
+            }
         }
     }
 
     //Por fim, todas as vacinas restantes são aplicadas como primeira dose
-    if(d1 <= lote){
+    if (lote > 0) {
         d1 += lote; //aplica o lote inteiro
-        console.log("aplica o lote inteiro como primeira dose");
     }
 }
