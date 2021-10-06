0d75c2db11c1f716b85ab8c8653cdc500ca4c37e
Aluno <reenyelima@hotmail.com>
2021-10-06 08:03:06 -0300

    2des bcd aula10

--- 2des/pwbe/workspace/delivery/deliverysql/src/main/java/domains/Entregador.java
+++ 2des/pwbe/workspace/delivery/deliverysql/src/main/java/domains/Entregador.java
@@ -1,7 +1,72 @@
-package controllers;
+package domains;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Entregador {
 	
+	//Atributos Básicos
+	private int idEntregador;
+	private String nomeCompleto;
+	private String veiculo;
+	
+	//Construtores - vazio
+	public Entregador() {
+	}
 
+	//Construtores - Cheio
+	public Entregador(int idEntregador, String nomeCompleto, String veiculo) {
+		this.idEntregador = idEntregador;
+		this.nomeCompleto = nomeCompleto;
+		this.veiculo = veiculo;
+	}
+	
+	//Construtores - Cheio String
+	public Entregador(String idEntregador, String nomeCompleto, String veiculo) {
+		this.idEntregador = Integer.valueOf(idEntregador);
+		this.nomeCompleto = nomeCompleto;
+		this.veiculo = veiculo;
+	}
+
+	//Getters && Setters
+	public int getIdEntregador() {
+		return idEntregador;
+	}
+
+	public void setIdEntregador(int idEntregador) {
+		this.idEntregador = idEntregador;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public String getVeiculo() {
+		return veiculo;
+	}
+
+	public void setVeiculo(String veiculo) {
+		this.veiculo = veiculo;
+	}
+
+	@Override
+	public String toString() {
+		return idEntregador + "\t" + nomeCompleto + "\t" + veiculo	+ "\n";
+	}
+	
+	public JSONObject toJSON() {
+		JSONObject json = new JSONObject();
+		try {
+			json.put("idEntregador", idEntregador);
+			json.put("nomeCompleto", nomeCompleto);
+			json.put("veiculo", veiculo);
+		} catch (JSONException e) {
+			System.out.println("Erro ao converter JSON: "+e);
+		}
+		return json;
+	}
 }
