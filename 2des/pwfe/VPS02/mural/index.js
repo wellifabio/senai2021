60e7651dc9d66bf6963b2b3c22fd152555455ff4
wellifabio <wellifabio@hotmail.com>
2021-12-10 10:18:10 -0300

    2des indmo revisao1

--- 2des/pwfe/VPS02/mural/index.js
+++ 2des/pwfe/VPS02/mural/index.js
@@ -5,14 +5,16 @@ const mural = document.querySelector('#mural')
 
 function add() {
     let card = document.createElement('div')
-    card.setAttribute("class", tipo.value)
-    card.innerHTML += '<button onclick="del(this)">X</button>'
-    card.innerHTML += '<a href="' + link.value + '" target="_blank">'
-    card.innerHTML += '<p>' + aviso.value + '</p>'
-    card.innerHTML += '</a>'
+    card.setAttribute("class", "card " + tipo.value)
+    card.innerHTML += '<div style="width:100%" align="right"><button class="fechar" onclick="del(this)">X</button></div>'
+    card.innerHTML += '<p><a href="' + link.value + '" target="_blank">'+aviso.value+'</a></p>'
     mural.appendChild(card)
 }
 
 function del(e){
-    e.parentNode.remove()
+    e.parentNode.parentNode.remove()
+}
+
+function estilizarInput(){
+    document.querySelector('#add').setAttribute("class",tipo.value)
 }
